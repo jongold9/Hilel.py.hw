3d6f52b58fc4e35d1dbb1df209e4eee95602c7cc
jongold9 <vadim.job1@gmail.com>
2022-06-10 18:41:30 +0300

    HW_1

---
+++ main.py
@@ -0,0 +1,22 @@
+#import this
+# вывод текста с рекомендациями от Tim Peters
+
+#import antigravity
+# ссылка на сайт с комиксом
+
+#import __hello__
+# вывод текста Hello world!
+
+#from __future__ import braces
+# пасхалка
+
+from  art  import  *
+tprint("Vadim",font="caligraphy")
+
+x = 4
+y = 2
+print(x, y)
+print(x+y)
+print(x//y)
+print(x%y)
+print(x**y)
